424c83cb422a4bb5f1bb1be23a406a18ae8d1fc8
h3nryprod01 <250794984+h3nryprod01@users.noreply.github.com>
2026-06-01 17:07:59 +0700

    release: v0.2.0 (app icon, click-through fix, Reveal Divider)
    Bump version to 0.2.0 and pin the Homebrew cask sha256 for the new build.

--- Casks/systembar.rb
+++ Casks/systembar.rb
@@ -1,6 +1,6 @@
 cask "systembar" do
-  version "0.1.0"
-  sha256 "35843e04dd648c0156fcbf6f0eac411fa0afa559ee41835839b5f9bc37e1e358"
+  version "0.2.0"
+  sha256 "eb068b0c8d172cbe190bc4d34ba3b4ae15d8708386e79423e10324f447e65259"
 
   url "https://github.com/h3nryprod01/systembar/releases/download/v#{version}/SystemBar.zip"
   name "SystemBar"
